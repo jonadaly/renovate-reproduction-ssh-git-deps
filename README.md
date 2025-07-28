94647a0c9244b0b69af0fd76a025726a23739c1c
Jon Daly <jon.daly@joinzoe.com>
2025-07-28 18:16:25 +0100

    Add lockfile

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Minimal reproducable example for renovate SSH git deps issue.
+
+To run:
+`$ uv run main.py`
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import httpx
+import requests
+
 def main():
     print("Hello from renovate-reproduction-ssh-git-deps!")
+    print(httpx.__version__)
+    print(requests.__version__)
 
 
 if __name__ == "__main__":
